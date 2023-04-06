f9989e5d37af778cb8809969172c140f78e557ba
TheQuantumMagician <102473092+TheQuantumMagician@users.noreply.github.com>
2023-04-06 18:36:12 -0400

    Add files via upload
    Added grayscale generation of the original image, as well as image smoothing (via averaging) and grayscale generation of that.
    
    Added command line override of averaging box edge size.

--- cli_edges.py
+++ cli_edges.py
@@ -41,6 +41,26 @@ def invert(pixel):
     return((r, g, b))
 
 
+def getAverage(im, x, y, size):
+    # calculate the average pixel value of a sizexsize square of pixels
+    # centered on x, y (or slightly off-centered if size is even)
+    # NOTE: Does no bounds checkig -- that's handld at the calling level
+    r, g, b = 0, 0, 0
+    margin = int(size / 2)
+    other_margin = (size - margin)
+    x_range = range(x - margin, x + other_margin)
+    y_range = range(y - margin, y + other_margin)
+    for newX in x_range:
+        for newY in y_range:
+            pixel = im.getpixel((newX, newY))
+            r += pixel[0]
+            g += pixel[1]
+            b += pixel[2]
+
+    pix_count = size * size
+    return((int(r / pix_count), int(g / pix_count), int(b / pix_count)))
+
+
 # background default
 background = (0, 0, 0, 255)
 
@@ -53,14 +73,20 @@ r_len = len(sd['red'])
 g_len = len(sd['green'])
 b_len = len(sd['blue'])
 
-colors = []    
+colors = []
+anticolors = []
 # Create a separate color for each palette entry from color map calculations.
 for col in range(0, palette_length):
     c_val = col / palette_length
     r = int(_find_color(c_val, sd['red'], r_len) * 255)
     g = int(_find_color(c_val, sd['green'], g_len) * 255)
     b = int(_find_color(c_val, sd['blue'], b_len) * 255)
-    colors.append((r, g, b))
+    color = (r, g, b)
+    colors.append(color)
+    anticolors.append(invert(color))
+
+# Create a grayscale lookup table
+grays = [(x, x, x) for x in range(palette_length)]
 
 # Instantiate the command line parser
 parser = argparse.ArgumentParser(description="edges: edge-enhancement and posterization application")
@@ -80,6 +106,13 @@ parser.add_argument('--th',
                     default = 16
                     )
 
+# Optional argument for edge threshhold (defaults to 16)
+parser.add_argument('--bs',
+                    type=int,
+                    help="override default averaging box edge size",
+                    default = 5
+                    )
+
 # Optional argument to display compute and display grayscale image
 parser.add_argument('--dgs', action='store_true', help="compute and dispaly grayscale")
 
@@ -91,6 +124,7 @@ print(args.fn)
 print(args.th)
 print(args.dgs)
 print(args.dp)
+print(args.bs)
 
 # open the file into an image object.
 im = Image.open(args.fn)
@@ -104,12 +138,40 @@ pPixels = pIm.load()
 iIm = Image.new('RGB', im.size, background)
 iPixels = iIm.load()
 
+# creat a canvas for grayscale version
+gIm = Image.new('RGB', im.size, background)
+gPixels = gIm.load()
+
 for x in range(0, im.size[0]):
     for y in range(0, im.size[1]):
-        pPixels[x, y] = colors[get_brightness(im.getpixel((x, y)))]
-        iPixels[x, y] = invert(pPixels[x, y])
-
-print([x for x in pPixels[0,0]])
+        bright = get_brightness(im.getpixel((x, y)))
+        pPixels[x, y] = colors[bright]
+        iPixels[x, y] = anticolors[bright]
+        gPixels[x, y] = grays[bright]
 
 pIm.show()
 iIm.show()
+gIm.show()
+
+# get averaged values
+# NOTE: margin/other_margin are used to allow average generation w/o bounds checking
+#       which speeds the whole process up considerably
+
+# create a canvas for the smoothed [averaged] version
+avgIm = Image.new('RGB', im.size, background)
+aPixels = avgIm.load()
+
+# create a canvas for the averaged grayscale version
+gAvgIm = Image.new('RGB', im.size, background)
+gaPixels = gAvgIm.load()
+
+margin = int(args.bs / 2)
+other_margin = args.bs - margin
+for x in range(margin, im.size[0] - other_margin):
+    for y in range(margin, im.size[1] - other_margin):
+        pixel = getAverage(im, x, y, args.bs)
+        aPixels[x, y] = pixel
+        gaPixels[x, y] = grays[get_brightness(pixel)]
+        
+avgIm.show()
+gAvgIm.show()
